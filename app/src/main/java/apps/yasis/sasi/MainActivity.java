b61612121648680a923981b20291650daabe16af
oyamoh-brian <oyamobrian8@gmail.com>
2019-12-11 13:30:39 -0800

    Initial Commit

---
+++ app/src/main/java/apps/yasis/sasi/MainActivity.java
@@ -0,0 +1,13 @@
+package apps.yasis.sasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
